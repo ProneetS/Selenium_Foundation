2baebc22a8d03a547955d12dc2fc31c664d9d533
ProneetS <proneetseth.office@gmail.com>
2024-04-25 11:15:37 +0530

    Used CSS Selectors to find elements and get the error text from console

--- Selenium_Step_by_Step/src/SeleniumLocatorsPractice.java
+++ Selenium_Step_by_Step/src/SeleniumLocatorsPractice.java
@@ -1,3 +1,5 @@
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,7 +10,7 @@ public class SeleniumLocatorsPractice {
 		// TODO Auto-generated method stub
 		
 		WebDriver driver = new ChromeDriver();
-		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		//hit url
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		
@@ -17,8 +19,16 @@ public class SeleniumLocatorsPractice {
 		driver.findElement(By.name("inputPassword")).sendKeys("kumar123"); //password
 		driver.findElement(By.className("signInBtn")).click(); //click on sign-in button
 		
+		//if you don't wait then it will fail again as it's taking some time to give that error.
+		//adding implicit wait
 		
+		
+		//here I am using CSS Selectors
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 
 	}
 
 }
+
+
+//from line 10 - 18, it explains login to the web-site is failed successfully. (1st part is successful)
