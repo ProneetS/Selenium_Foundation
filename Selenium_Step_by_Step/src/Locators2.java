5709072265988d25b64826591addad0fc7d530a1
ProneetS <proneetseth.office@gmail.com>
2024-05-02 13:33:27 +0530

    Collecting password from text dynamically from forgot password page

--- Selenium_Step_by_Step/src/Locators2.java
+++ Selenium_Step_by_Step/src/Locators2.java
@@ -14,10 +14,18 @@ public class Locators2 {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		
+		//calling the method
+		String password = getPassword(driver);
+		
+		//from parent to child traverse for 'go to login page' button 
+		driver.findElement(By.xpath("//div[@class=\"forgot-pwd-btn-conainer\"]/button[1]")).click();
+		
+		Thread.sleep(1000);
+		
 		String name = "Proneet";
 		//Signing with User ID and Password
 		driver.findElement(By.id("inputUsername")).sendKeys(name); 
-		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy"); 
+		driver.findElement(By.name("inputPassword")).sendKeys(password); 
 		driver.findElement(By.className("signInBtn")).click(); 
 		
 		Thread.sleep(2000);
@@ -38,6 +46,41 @@ public class Locators2 {
 		//close the browser
 		driver.close();
 
+	}
+	
+	public static String getPassword(WebDriver driver) throws InterruptedException //passing WebDriver driver as an argument for diver to be used
+	{
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		
+		//Forgot password (Linked Text)
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		
+		//putting waiting time to complete the rendering of the page after slide.
+		Thread.sleep(2000);
+		
+		//directly clicking on RESET Login button
+		driver.findElement(By.className("reset-pwd-btn")).click();
+		
+		//grabbing the highlighted text
+		String PasswordText = driver.findElement(By.cssSelector("form p")).getText();
+		
+		//text we got " Please use temporary password 'rahulshettyacademy' to Login. "
+		//Splitting the text based on ' (single quote)
+		String[] PasswordArray = PasswordText.split("'");
+		// PasswordArray[0] = Please use temporary password 
+		// PasswordArray[1] = rahulshettyacademy' to Login.
+		
+		//split again to get the password text
+		String[] PasswordArray2 = PasswordArray[1].split("'");
+		// PasswordArray2[0] = rahulshettyacademy
+		// PasswordArray2[1] = to Login.
+		//you can extract 0th index in another way also. PasswordArray[1].split("'")[0]
+		
+		//storing the password in a variable
+		String Password = PasswordArray2[0];
+		return Password;
+		
+		
 	}
 
 }
