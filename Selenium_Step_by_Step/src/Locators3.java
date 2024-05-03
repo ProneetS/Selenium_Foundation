c9b719a0a6111f849100ff3ea0546f455b52a2a0
ProneetS <proneetseth.office@gmail.com>
2024-05-03 17:44:53 +0530

    Traverse from child to parent

--- Selenium_Step_by_Step/src/Locators3.java
+++ Selenium_Step_by_Step/src/Locators3.java
@@ -10,8 +10,11 @@ public class Locators3 {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 		
-		//click on login button (Note: XPath is written with Paren to Child and Sibling to sibling mix
+		//click on login button (Note: XPath is written with Parent to Child and Sibling to sibling mix
 		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+		
+		//traverse from child to parent
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[3]")).getText());
 
 	}
 
