3f2a2d9a3fd79c3d358674c5a2ac42c72601d98e
ProneetS <proneetseth.office@gmail.com>
2024-05-08 23:16:45 +0530

    Updated drop down selecting multiple person

---
+++ Selenium_Step_by_Step/src/UpdatedDropDown.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UpdatedDropDown {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		//Invoking Chrome Browser
+		WebDriver driver = new ChromeDriver();
+		
+		//visit url
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		//find out Passengers tab to select 5 adults
+		driver.findElement(By.id("divpaxinfo")).click();
+		
+		Thread.sleep(1000);
+		
+		//getting the text to verify before selecting the passenger
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		
+		//looping to select 5 adults
+		for(int i = 1; i < 5; i++) {
+		driver.findElement(By.id("hrefIncAdt")).click();
+		}
+		
+		//clicking on done button
+		driver.findElement(By.id("btnclosepaxoption")).click();
+		
+		//getting the text to verify after selecting the passenger
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+	}
+
+}
