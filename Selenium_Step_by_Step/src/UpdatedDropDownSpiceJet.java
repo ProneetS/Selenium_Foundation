6c4a99d05be170b91dafc218e592307e8fbd5810
ProneetS <proneetseth.office@gmail.com>
2024-05-08 23:36:59 +0530

    Updated drop down Spicejet web-site

---
+++ Selenium_Step_by_Step/src/UpdatedDropDownSpiceJet.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UpdatedDropDownSpiceJet {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		//Invoking Chrome Browser
+		WebDriver driver = new ChromeDriver();
+				
+		//visit url
+		driver.get("https://www.spicejet.com/");
+		
+		driver.findElement(By.xpath("//div[@data-testid=\"home-page-travellers\"]")).click();
+		
+		System.out.println(driver.findElement(By.xpath("//div[@data-testid=\"home-page-travellers\"]")).getText());
+		
+		Thread.sleep(1000);
+		
+		for(int i = 1; i < 5; i++){
+		driver.findElement(By.xpath("//div[@data-testid=\"Adult-testID-plus-one-cta\"]")).click();
+		}
+		
+		driver.findElement(By.xpath("//div[@data-testid=\"home-page-travellers-done-cta\"]")).click();
+		
+		System.out.println(driver.findElement(By.xpath("//div[@data-testid=\"home-page-travellers\"]")).getText());
+	}
+
+}
