5de68d2dc5409343310f7544fa646a98f8cf965a
ProneetS <proneetseth.office@gmail.com>
2024-05-09 10:28:02 +0530

    handling dynamic drop downs

---
+++ Selenium_Step_by_Step/src/Dropdowns/DynamicDropDown.java
@@ -0,0 +1,31 @@
+package Dropdowns;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DynamicDropDown {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		//invoking chrome browser
+		WebDriver driver = new ChromeDriver();
+		
+		//open the url
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		//click on the "From" drop down 
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		
+		//selecting the city from "From" drop down
+		driver.findElement(By.xpath("//a[@value='JRG']")).click();
+		
+		Thread.sleep(2000);
+		
+		//selecting the city from "To" drop down which will enable after selecting city from "From"
+		driver.findElement(By.xpath("(//a[@value='BLR'])[2]")).click();
+		
+		
+	}
+
+}
