3d5488323f01b87a3cf9a39adaba0ee22a2c587b
ProneetS <proneetseth.office@gmail.com>
2024-05-09 16:26:05 +0530

    Parent Child relationship XPath

---
+++ Selenium_Step_by_Step/src/Dropdowns/DynamicDropDownParentChildReln.java
@@ -0,0 +1,31 @@
+package Dropdowns;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DynamicDropDownParentChildReln {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		//invoking Chrome Browser
+		WebDriver driver = new ChromeDriver();
+		
+		//visit url
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		//click on the "From" drop down 
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		
+		//finding "From" drop down city in parent child relationship XPath
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='JRG']")).click();
+		
+		Thread.sleep(2000);
+		
+		//finding "To" drop down city in parent child relationship XPath
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='BLR']")).click();
+		
+
+	}
+
+}
