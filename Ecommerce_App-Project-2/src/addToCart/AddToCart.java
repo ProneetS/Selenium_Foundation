28e58b70fdd0f5121cbdf049a34320ee80a2dede
ProneetS <proneetseth.office@gmail.com>
2024-05-15 07:06:48 +0530

    This code will find "cucumber" item and click on "add to cart" button.

--- Ecommerce_App-Project-2/src/addToCart/AddToCart.java
+++ Ecommerce_App-Project-2/src/addToCart/AddToCart.java
@@ -1,14 +1,34 @@
 package addToCart;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AddToCart {
 
 	public static void main(String[] args) {
 		
+		//invoking the chrome browser
+		WebDriver driver = new ChromeDriver();
 		
+		//visit the url
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 		
+		//storing all the items to a list
+		List<WebElement> products = driver.findElements(By.className("h4.product-name"));
+		
+		//iterating through all the product to find the location/index of cucumber
+		for(int i = 0; i < products.size(); i++) {
+			String name = products.get(i).getText();
+			
+			if(name.contains("Cucumber")) {
+				//click on add to cart
+				driver.findElements(By.xpath("//button[text()='ADD TO CART']"));
+			}
+		}
 
 	}
 
