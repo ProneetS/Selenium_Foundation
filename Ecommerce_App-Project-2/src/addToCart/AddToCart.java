fbe70a493485f96bf3edcf6267dce72fc6b8c21b
ProneetS <proneetseth.office@gmail.com>
2024-05-15 06:58:07 +0530

    added dependencies and created a class for AddToCart

---
+++ Ecommerce_App-Project-2/src/addToCart/AddToCart.java
@@ -0,0 +1,15 @@
+package addToCart;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AddToCart {
+
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+
+}
