f5b04758e378e3264c26f7bae22a510955bbe3dd
ProneetS <proneetseth.office@gmail.com>
2024-05-16 14:24:03 +0530

    Optimized code by creating separate method for adding items of Ecom-App

---
+++ Selenium_Step_by_Step/src/synchronization_Or_Waits/Synchronization.java
@@ -0,0 +1,67 @@
+package synchronization_Or_Waits;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Synchronization {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		// invoking the chrome browser
+		WebDriver driver = new ChromeDriver();
+
+		// declaring vegetables array that we need
+		String[] veggies = { "Cucumber", "Brocolli", "Beetroot" };
+	
+
+		// visit the url
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		Thread.sleep(3000);
+		addItems(driver, veggies);
+
+	}
+
+	public static void addItems(WebDriver driver, String[] veggies) {
+		
+		int count = 0;
+		
+		// storing all the items to a list
+		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+
+		
+		// iterating through all the product to find the location/index of cucumber
+		for (int i = 0; i < products.size(); i++) {
+
+			// Format the vegetable name from “Brocolli – 1 Kg” to “Brocolli” using split()
+			// method
+			String[] name = products.get(i).getText().split("-");
+
+			// removing extra space after the actual veggie name.
+			String formattedName = name[0].trim();
+
+			// converting array to array-list in run time
+			List itemsNeededList = Arrays.asList(veggies);
+
+			// if the name that we are getting is present in the list, then go inside
+			if (itemsNeededList.contains(formattedName)) {
+
+				count++;
+
+				// click on add to cart whose i value is cucumber
+				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+
+				// adding break after found 3 veggies mentioned in array
+				if (count == veggies.length) {
+					break;
+				}
+
+			}
+		}
+	}
+
+}
