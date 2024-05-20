1ef3243fd28a17a83808f1482b84b7d5f0c6ae79
ProneetS <proneetseth.office@gmail.com>
2024-05-20 17:53:34 +0530

    Perform Right click

---
+++ Selenium_Step_by_Step/src/actionClassDemo/PerformRightClick.java
@@ -0,0 +1,30 @@
+package actionClassDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class PerformRightClick {
+
+	public static void main(String[] args) {
+
+		// invoking browser
+		WebDriver driver = new ChromeDriver();
+
+		// visit the amazon.com
+		driver.get("https://www.amazon.in/");
+
+		// create Action class object.
+		Actions a = new Actions(driver);
+		
+		//perform mouse hover action
+		WebElement move = driver.findElement(By.cssSelector("a[id='nav-link-accountList']"));
+
+		//  right click on the element and use build() and perform() method to execute it
+		a.moveToElement(move).contextClick().build().perform();
+
+	}
+
+}
