91515b0d012aaede9dc3a9366d4b9e6938dcbf91
ProneetS <proneetseth.office@gmail.com>
2024-05-22 07:03:48 +0530

    Window Handling assignment

---
+++ Selenium_Step_by_Step/src/Handling_Windows/AssignmentWindowHandle.java
@@ -0,0 +1,58 @@
+package Handling_Windows;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AssignmentWindowHandle {
+
+	public static void main(String[] args) {
+		
+		//invoking browser
+		WebDriver driver = new ChromeDriver();
+		
+		//visit url
+		driver.get("https://the-internet.herokuapp.com/");
+		
+		//maximize the window
+		driver.manage().window().maximize();
+		
+		//selecting "Multiple Window" from the list
+		//driver.findElement(By.xpath("//*[contains(text(),'Multiple Windows')]")).click();
+		driver.findElement(By.linkText("Multiple Windows")).click();
+		
+		//Clicking on "Click Here"
+		//driver.findElement(By.xpath("//*[contains(text(),'Click Here')]")).click();
+		driver.findElement(By.linkText("Click Here")).click();
+		
+		//getting asll the window opened data
+		Set<String> Wind = driver.getWindowHandles();
+		
+		//iterating through the wind value
+		Iterator<String> it = Wind.iterator();
+		
+		//fetching parent and child window
+		String parentWind = it.next();
+		String childWind = it.next();
+		
+		//switching to child window
+		driver.switchTo().window(childWind);
+		
+		//Grabbing the text and printing it in output
+		System.out.println(driver.findElement(By.cssSelector("div[class='example']")).getText());
+		
+		//return back to parent window
+		driver.switchTo().window(parentWind);
+		
+		//grabbing the text and printing it on output
+		System.out.println(driver.findElement(By.cssSelector("div[class='example'] h3")).getText());
+		
+		//closing all windows
+		driver.quit();
+		
+	}
+
+}
