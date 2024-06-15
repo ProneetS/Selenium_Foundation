7e82f08a8d59d0049f0f6f2cc5e885f1c62dd983
ProneetS <proneetseth.office@gmail.com>
2024-06-15 10:02:23 +0530

    Move to Tables, scroll, store WebElmnt in List, get values, sum them up

--- Selenium_Step_by_Step/src/scrollWebPages/ScrollWebPage.java
+++ Selenium_Step_by_Step/src/scrollWebPages/ScrollWebPage.java
@@ -1,7 +1,11 @@
 package scrollWebPages;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ScrollWebPage {
@@ -21,7 +25,17 @@ public class ScrollWebPage {
 		//Table level scrolling present inside window.
 		js.executeScript("document.querySelector(\".tableFixHead\").scrollTop=5000");
 		
+		//store the WebElements inside a list of this Table, (identifying the required table as parent child relationship) 
+		List<WebElement> values = driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
 		
+		//Traverse through all WebElements and get their values
+		int sum = 0;
+		for(int i = 0; i < values.size(); i++) {
+			//convert string to integer
+			sum = sum + Integer.parseInt(values.get(i).getText());
+		}
+		
+		System.out.println(sum);
 
 	}
 
